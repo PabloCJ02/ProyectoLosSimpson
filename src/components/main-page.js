c259c43b2c50b89180b79dde002350bbbab068d5
PabloCJ02 <thesorkamax0001@gmail.com>
2021-10-28 09:06:56 -0500

    segundo commit

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -16,43 +16,34 @@ export const MainPage = () => {
 
   return (
     <p>
-    <main class="gradient-border">
+      <main class="gradient-border">
       <div class="box">
-      <img class="img-responsive center-block" src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2Fsimpsons.PNG?1497481539770"></img>
-        <div class="container">
-          <div class="hair-top"></div>
-          <div class="hair-top left"></div>
-          <div class="forehead"></div>
-          <div class="eye">
-            <div class="pupil right"></div>
+        <img class="img-responsive center-block" src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2Fsimpsons.PNG?1497481539770"></img>
+          <div class="texto">
+          {isLoading && <p>Loading</p>}
+          <p>The simpsooon!!!</p>
+          <ul>
+            {quotes.map(({ character }) => (
+              <p key={character} style={{ fontSize: '25px', textAlign: 'center'}} > <strong>{character} says... </strong> </p>
+            ))}
+          </ul>
+
+          <ul>
+            {quotes.map(({ quote }) => (
+              <p style={{ fontSize: '20px', textAlign: 'center', color: 'black'}} key={quote} > <strong>{quote}</strong></p>
+            ))}
+          </ul>
+
+          <ul>
+            {quotes.map(({ image, character }) => (
+              <div style={{ textAlign:'center'}}>
+                <img key={character} src={image} alt={character} style={{ width: '100', height: '100'}}></img>
+              </div>
+            ))}
+          </ul>
           </div>
-          <div class="eye left">
-            <div class="pupil"></div>
-          </div>
-          <div class="nose"></div>
-          <div class="cheeck"></div>
-          <div class="beard"></div>
-          <div class="smile"></div>
-          <div class="neck"></div>
-          <div class="neck-bottom"></div>
-          <div class="v-neck"></div>
-          <div class="shirt-left"></div>
-          <div class="shirt-right"></div>
-          <div class="body"></div>
-          <div class="shoulder-left"></div>
-          <div class="shoulder-right"></div>
         </div>
-        <div class="texto">
-        {isLoading && <p>Loading</p>}
-        <p>The simpsooon!!!</p>
-        <ul>
-          {quotes.map(({quote}) => (
-            <li key={quote}>{quote}</li>
-          ))}
-        </ul>
-        </div>
-      </div>
-    </main>
+      </main>
     </p>
   )
 }
